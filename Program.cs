9f41534f49df353b39878c8d0f94374ed7670e53
Anton <ttarasovanton@ya.ru>
2023-02-28 19:12:01 +0600

    a method to fill an array added

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿string[] fillarr(int size) 
+{ 
+    string[] arr = new string[size]; 
+    Console.WriteLine("Введите элементы массива через enter"); 
+    for (int i = 0; i < size;) 
+    { 
+        string num = Console.ReadLine(); 
+        arr[i] = num; 
+        i++; 
+        if (size-i !=0) 
+        { 
+            Console.WriteLine($"Вы еще можете ввести {size-i} элементов"); 
+        } 
+        else 
+        { 
+            Console.WriteLine($"Все"); 
+        } 
+    } 
+    return arr; 
+} 
